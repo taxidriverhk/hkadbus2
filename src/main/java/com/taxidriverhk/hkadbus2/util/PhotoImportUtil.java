bb5024e009cf451d6af79c6bb000efd1b2129937
Alex Leung <taxidriverhk@gmail.com>
2023-03-06 13:26:56 -0800

    Support photo import from an Excel file

--- src/main/java/com/taxidriverhk/hkadbus2/util/PhotoImportUtil.java
+++ src/main/java/com/taxidriverhk/hkadbus2/util/PhotoImportUtil.java
@@ -2,6 +2,7 @@ package com.taxidriverhk.hkadbus2.util;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
@@ -19,10 +20,19 @@ import com.taxidriverhk.hkadbus2.module.DataAccessModule;
 import lombok.Builder;
 import lombok.Data;
 import lombok.SneakyThrows;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -40,6 +50,11 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class PhotoImportUtil {
+
+    public static enum PhotoImportFileType {
+        CSV,
+        EXCEL;
+    }
 
     @Builder
     @Data
@@ -100,57 +115,14 @@ public class PhotoImportUtil {
     }
 
     public void execute() throws Exception {
-        execute(INPUT_FILE_PATH);
+        execute(INPUT_FILE_PATH, PhotoImportFileType.CSV);
     }
 
     @VisibleForTesting
-    public List<PhotoEntity> execute(final String inputFilePath) throws Exception {
-        // Read csv file with data
-        // Each row has the following attributes
-        // [
-        //      category_name_en,category_name_zh,category_hash_key,advertisement_name_en,advertisement_name_zh,advertisement_hash_key,
-        //      bus_brand_name_en,bus_brand_name_zh,bus_brand_hash_key,bus_model_name_en,bus_model_name_zh,bus_model_hash_key,
-        //      license_plate_number,fleet_prefix,fleet_number,bus_company,
-        //      bus_route_hash_key,bus_route_number,bus_companies,start_en,start_zh,end_en,end_zh,
-        //      username,
-        //      thumbnail,image,
-        // ]
-        final List<String> lines = Files.readAllLines(Paths.get(inputFilePath));
-        // Remove the header line
-        lines.remove(0);
-        final List<PhotoImportEntry> photoImportEntries = lines.stream()
-                .map(line -> {
-                    final String[] tokens = line.split(",");
-                    return PhotoImportEntry.builder()
-                            .categoryNameEn(tokens[0])
-                            .categoryNameZh(tokens[1])
-                            .categoryHashKey(tokens[2])
-                            .advertisementNameEn(tokens[3])
-                            .advertisementNameZh(tokens[4])
-                            .advertisementHashKey(tokens[5])
-                            .busBrandNameEn(tokens[6])
-                            .busBrandNameZh(tokens[7])
-                            .busBrandHashKey(tokens[8])
-                            .busModelNameEn(tokens[9])
-                            .busModelNameZh(tokens[10])
-                            .busModelHashKey(tokens[11])
-                            .licensePlateNumber(tokens[12])
-                            .fleetPrefix(tokens[13])
-                            .fleetNumber(tokens[14])
-                            .busCompany(tokens[15])
-                            .busRouteHashKey(tokens[16])
-                            .busRouteNumber(tokens[17])
-                            .busRouteCompanies(tokens[18].split("\\|"))
-                            .startEn(tokens[19])
-                            .startZh(tokens[20])
-                            .endEn(tokens[21])
-                            .endZh(tokens[22])
-                            .username(tokens[23])
-                            .thumbnail(tokens[24])
-                            .image(tokens[25])
-                            .build();
-                })
-                .collect(Collectors.toList());
+    public List<PhotoEntity> execute(final String inputFilePath, final PhotoImportFileType fileType) throws Exception {
+        final List<PhotoImportEntry> photoImportEntries = fileType == PhotoImportFileType.CSV
+            ? readFromCsv(inputFilePath)
+            : readFromExcel(inputFilePath);
 
         final Session session = sessionFactory.openSession();
         // Insert categories (keyed by category hash key)
@@ -397,6 +369,113 @@ public class PhotoImportUtil {
         insertSearchRecordTransactions.commit();
 
         return photoEntities;
+    }
+
+    @SneakyThrows
+    private List<PhotoImportEntry> readFromCsv(final String inputFilePath) {
+        // Read csv file with data
+        // Each row has the following attributes
+        // [
+        //      category_name_en,category_name_zh,category_hash_key,advertisement_name_en,advertisement_name_zh,advertisement_hash_key,
+        //      bus_brand_name_en,bus_brand_name_zh,bus_brand_hash_key,bus_model_name_en,bus_model_name_zh,bus_model_hash_key,
+        //      license_plate_number,fleet_prefix,fleet_number,bus_company,
+        //      bus_route_hash_key,bus_route_number,bus_companies,start_en,start_zh,end_en,end_zh,
+        //      username,
+        //      thumbnail,image,
+        // ]
+        final List<String> lines = Files.readAllLines(Paths.get(inputFilePath));
+        // Remove the header line
+        lines.remove(0);
+        return lines.stream()
+            .map(line -> {
+                final String[] tokens = line.split(",");
+                return PhotoImportEntry.builder()
+                        .categoryNameEn(tokens[0])
+                        .categoryNameZh(tokens[1])
+                        .categoryHashKey(tokens[2])
+                        .advertisementNameEn(tokens[3])
+                        .advertisementNameZh(tokens[4])
+                        .advertisementHashKey(tokens[5])
+                        .busBrandNameEn(tokens[6])
+                        .busBrandNameZh(tokens[7])
+                        .busBrandHashKey(tokens[8])
+                        .busModelNameEn(tokens[9])
+                        .busModelNameZh(tokens[10])
+                        .busModelHashKey(tokens[11])
+                        .licensePlateNumber(tokens[12])
+                        .fleetPrefix(tokens[13])
+                        .fleetNumber(tokens[14])
+                        .busCompany(tokens[15])
+                        .busRouteHashKey(tokens[16])
+                        .busRouteNumber(tokens[17])
+                        .busRouteCompanies(tokens[18].split("\\|"))
+                        .startEn(tokens[19])
+                        .startZh(tokens[20])
+                        .endEn(tokens[21])
+                        .endZh(tokens[22])
+                        .username(tokens[23])
+                        .thumbnail(tokens[24])
+                        .image(tokens[25])
+                        .build();
+            })
+            .collect(Collectors.toList());
+    }
+
+    @SneakyThrows
+    private List<PhotoImportEntry> readFromExcel(final String inputFilePath) {
+        final FileInputStream excelFile = new FileInputStream(new File(inputFilePath));
+        final Workbook workbook = new XSSFWorkbook(excelFile);
+
+        final Sheet sheet = workbook.getSheetAt(0);
+        final List<PhotoImportEntry> entries = Lists.newLinkedList();
+        for (final Row row : sheet) {
+            // Skip the header
+            if (row.getRowNum() == 0) {
+                continue;
+            }
+
+            final PhotoImportEntry entry = PhotoImportEntry.builder()
+                    .categoryNameEn(getStringExcelCellValue(row, 0))
+                    .categoryNameZh(getStringExcelCellValue(row, 1))
+                    .categoryHashKey(getStringExcelCellValue(row, 2))
+                    .advertisementNameEn(getStringExcelCellValue(row, 3))
+                    .advertisementNameZh(getStringExcelCellValue(row, 4))
+                    .advertisementHashKey(getStringExcelCellValue(row, 5))
+                    .busBrandNameEn(getStringExcelCellValue(row, 6))
+                    .busBrandNameZh(getStringExcelCellValue(row, 7))
+                    .busBrandHashKey(getStringExcelCellValue(row, 8))
+                    .busModelNameEn(getStringExcelCellValue(row, 9))
+                    .busModelNameZh(getStringExcelCellValue(row, 10))
+                    .busModelHashKey(getStringExcelCellValue(row, 11))
+                    .licensePlateNumber(getStringExcelCellValue(row, 12))
+                    .fleetPrefix(getStringExcelCellValue(row, 13))
+                    .fleetNumber(getStringExcelCellValue(row, 14))
+                    .busCompany(getStringExcelCellValue(row, 15))
+                    .busRouteHashKey(getStringExcelCellValue(row, 16))
+                    .busRouteNumber(getStringExcelCellValue(row, 17))
+                    .busRouteCompanies(getStringExcelCellValue(row, 18).split("\\|"))
+                    .startEn(getStringExcelCellValue(row, 19))
+                    .startZh(getStringExcelCellValue(row, 20))
+                    .endEn(getStringExcelCellValue(row, 21))
+                    .endZh(getStringExcelCellValue(row, 22))
+                    .username(getStringExcelCellValue(row, 23))
+                    .thumbnail(getStringExcelCellValue(row, 24))
+                    .image(getStringExcelCellValue(row, 25))
+                    .build();
+            entries.add(entry);
+        }
+
+        workbook.close();
+        return entries;
+    }
+
+    private String getStringExcelCellValue(final Row row, final int index) {
+        final Cell cell = row.getCell(index);
+        if (cell.getCellType() == CellType.NUMERIC) {
+            return new Integer((int) cell.getNumericCellValue()).toString();
+        }
+
+        return row.getCell(index).getStringCellValue();
     }
 
     @SneakyThrows
--- src/test/java/com/taxidriverhk/hkadbus2/util/PhotoImportUtilTest.java
+++ src/test/java/com/taxidriverhk/hkadbus2/util/PhotoImportUtilTest.java
@@ -9,15 +9,20 @@ import com.taxidriverhk.hkadbus2.provider.impl.SearchPhotoProviderImpl;
 import com.taxidriverhk.hkadbus2.repository.PhotoRepository;
 import com.taxidriverhk.hkadbus2.repository.impl.PhotoSqlRepository;
 import com.taxidriverhk.hkadbus2.repository.impl.SqlRepositoryTestBase;
+import com.taxidriverhk.hkadbus2.util.PhotoImportUtil.PhotoImportFileType;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.net.URL;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static com.taxidriverhk.hkadbus2.util.MockDataHelper.ADVERTISEMENT_ENTITY_1;
 import static com.taxidriverhk.hkadbus2.util.MockDataHelper.CATEGORY_ENTITY_1;
@@ -29,6 +34,7 @@ import static com.taxidriverhk.hkadbus2.util.MockDataHelper.USER_ENTITY_1;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class PhotoImportUtilTest extends SqlRepositoryTestBase {
 
@@ -43,8 +49,12 @@ public class PhotoImportUtilTest extends SqlRepositoryTestBase {
         searchPhotoProvider = new SearchPhotoProviderImpl(sessionFactory);
     }
 
-    @Test
-    public void GIVEN_inputFile_WHEN_import_THEN_shouldInsertEntitiesIntoDatabase() throws Exception {
+    @ParameterizedTest
+    @MethodSource("photoImportTestCases")
+    public void GIVEN_inputFileInCsvFormat_WHEN_import_THEN_shouldInsertEntitiesIntoDatabase(
+            final String inputFilePath,
+            final PhotoImportFileType importFileType
+    ) throws Exception {
         // Insert a single bus entity to check to ensure the duplicate check works
         final Session session = sessionFactory.openSession();
         final Transaction insertBusTransaction = session.beginTransaction();
@@ -59,8 +69,8 @@ public class PhotoImportUtilTest extends SqlRepositoryTestBase {
         insertBusTransaction.commit();
 
         // Import the CSV file
-        final URL testFileUrl = getClass().getClassLoader().getResource("test-photo-import-file.csv");
-        final List<PhotoEntity> photoEntities = photoImportUtil.execute(Paths.get(testFileUrl.toURI()).toString());
+        final URL testFileUrl = getClass().getClassLoader().getResource(inputFilePath);
+        final List<PhotoEntity> photoEntities = photoImportUtil.execute(Paths.get(testFileUrl.toURI()).toString(), importFileType);
 
         // Verify that the photo entity is found
         final Optional<PhotoEntity> photoEntity = photoRepository.getPhotoByShortId(photoEntities.get(0).getShortId());
@@ -75,6 +85,18 @@ public class PhotoImportUtilTest extends SqlRepositoryTestBase {
                 10);
         assertThat(searchRecordResult.getSearchRecordEntities().size(), equalTo(10));
         assertThat(searchRecordResult.getTotal(), greaterThanOrEqualTo(1L));
+    }
+
+    public static Stream<Arguments> photoImportTestCases() {
+        return Stream.of(
+                arguments(
+                    "test-photo-import-file.csv",
+                    PhotoImportFileType.CSV
+                ),
+                arguments(
+                    "test-photo-import-file.xlsx",
+                    PhotoImportFileType.EXCEL
+                ));
     }
 
     @Override
