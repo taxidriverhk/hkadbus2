bfc05aa6591ca7f1cf959eec92722281aed3b93e
Alex Leung <taxidriverhk@gmail.com>
2024-07-28 21:39:16 -0700

    Removed old frontend code files

--- frontend-LEGACY/src/photo/utils/bus-company-color/index.js
+++
@@ -1,8 +0,0 @@
-export default function getColorByBusCompany(busCompany) {
-  return {
-    kmb: 'red',
-    ctb: 'yellow',
-    nwfb: 'green',
-    cmb: 'light_blue',
-  }[busCompany.toLowerCase()] || 'default';
-}
--- frontend-LEGACY/src/shared/store/constants.js
+++
@@ -1,3 +0,0 @@
-export const IMAGE_BASE_PATH = 'images/';
-
-export const DATE_FORMAT = 'yyyy-MM-dd';
